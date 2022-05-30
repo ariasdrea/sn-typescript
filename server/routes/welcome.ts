e440b7350bfd45b3f1e9f6083c462117d48c962c
Andrea Arias <ariasdrea@gmail.com>
2022-05-30 11:46:53 +0200

    adding express router to compartmentalize the routes

---
+++ server/routes/welcome.ts
@@ -0,0 +1,91 @@
+import express from "express";
+const router = express.Router();
+import cryptoRandomString from "crypto-random-string";
+import { genSalt, hash } from "bcryptjs";
+import {
+    registerUser,
+    getUserInfoByEmail,
+    loginUser,
+    storeCode,
+    retrieveCode,
+    updatePassword,
+} from "../db";
+import { sendEmail } from "../ses";
+
+router.get("/userInfo.json", async (req, res) => {
+    res.json({
+        userId: req.session.userId,
+    });
+});
+
+router.post("/register.json", async (req, res) => {
+    try {
+        const userId = await registerUser(req.body);
+        req.session.userId = userId;
+        res.json({ success: true });
+    } catch (e) {
+        console.error("e in /register.json", e);
+        res.json({ success: false });
+    }
+});
+
+router.post("/login.json", async (req, res) => {
+    try {
+        const { id: userId, password: hashedPassword } =
+            await getUserInfoByEmail(req.body.email);
+        const match = await loginUser(req.body.password, hashedPassword);
+
+        if (match) {
+            req.session.userId = userId;
+            res.json({ success: true });
+        }
+    } catch (e) {
+        console.log("e in /login.json", e);
+        res.json({ success: false });
+    }
+});
+
+router.get("/logout", (req, res) => {
+    req.session = null;
+    res.redirect("/");
+});
+
+router.post("/reset.json", async (req, res) => {
+    try {
+        const { id } = await getUserInfoByEmail(req.body.email);
+
+        if (id) {
+            const secretCode = cryptoRandomString({ length: 6 });
+            const code = await storeCode(secretCode, req.body.email);
+            await sendEmail(code, req.body.email);
+
+            res.json({ success: true });
+        }
+    } catch (e) {
+        console.log("e in reset.json", e);
+        res.json({ success: true });
+    }
+});
+
+router.post("/reset-verify.json", async (req, res) => {
+    const { email, code, password } = req.body;
+    try {
+        const result = await retrieveCode(email);
+        const match = result.find((elem) => elem.code === code);
+
+        if (match) {
+            const salt = await genSalt();
+            const hashedPassword = await hash(password, salt);
+            await updatePassword(email, hashedPassword);
+
+            res.json({ success: true });
+        } else {
+            res.json({ success: false });
+        }
+    } catch (e) {
+        console.log("e in reset-verify.json", e);
+        res.json({ success: false });
+    }
+});
+
+export default router;
--- server/server.ts
+++ server/server.ts
@@ -2,17 +2,7 @@ import express, { Express } from "express";
 import compression from "compression";
 import cookieSession from "cookie-session";
 import path from "path";
-import {
-    registerUser,
-    getUserInfoByEmail,
-    loginUser,
-    storeCode,
-    retrieveCode,
-    updatePassword,
-} from "./db";
-import { sendEmail } from "./ses";
-import cryptoRandomString from "crypto-random-string";
-import { genSalt, hash } from "bcryptjs";
+import welcomeRouter from "./routes/welcome";
 
 const app: Express = (exports.app = express());
 const cookieSessionMiddleware = cookieSession({
@@ -27,81 +17,7 @@ app.use(express.json());
 app.disable("x-powered-by");
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
-app.get("/userInfo.json", async (req, res) => {
-    res.json({
-        userId: req.session.userId,
-    });
-});
-
-app.post("/register.json", async (req, res) => {
-    try {
-        const userId = await registerUser(req.body);
-        req.session.userId = userId;
-        res.json({ success: true });
-    } catch (e) {
-        console.error("e in /register.json", e);
-        res.json({ success: false });
-    }
-});
-
-app.post("/login.json", async (req, res) => {
-    try {
-        const { id: userId, password: hashedPassword } =
-            await getUserInfoByEmail(req.body.email);
-        const match = await loginUser(req.body.password, hashedPassword);
-
-        if (match) {
-            req.session.userId = userId;
-            res.json({ success: true });
-        }
-    } catch (e) {
-        console.log("e in /login.json", e);
-        res.json({ success: false });
-    }
-});
-
-app.get("/logout", (req, res) => {
-    req.session = null;
-    res.redirect("/");
-});
-
-app.post("/reset.json", async (req, res) => {
-    try {
-        const { id } = await getUserInfoByEmail(req.body.email);
-
-        if (id) {
-            const secretCode = cryptoRandomString({ length: 6 });
-            const code = await storeCode(secretCode, req.body.email);
-            await sendEmail(code, req.body.email);
-
-            res.json({ success: true });
-        }
-    } catch (e) {
-        console.log("e in reset.json", e);
-        res.json({ success: true });
-    }
-});
-
-app.post("/reset-verify.json", async (req, res) => {
-    const { email, code, password } = req.body;
-    try {
-        const result = await retrieveCode(email);
-        const match = result.find((elem) => elem.code === code);
-
-        if (match) {
-            const salt = await genSalt();
-            const hashedPassword = await hash(password, salt);
-            await updatePassword(email, hashedPassword);
-
-            res.json({ success: true });
-        } else {
-            res.json({ success: false });
-        }
-    } catch (e) {
-        console.log("e in reset-verify.json", e);
-        res.json({ success: false });
-    }
-});
+app.use("/", welcomeRouter);
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "client", "index.html"));
