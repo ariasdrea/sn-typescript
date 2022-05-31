414a3c0fcf88a5247ff096eb8e6b670438a3e1a4
Andrea Arias <ariasdrea@gmail.com>
2022-05-31 18:03:11 +0200

    modifying import and removing unnecessary typing

--- server/routes/welcome.ts
+++ server/routes/welcome.ts
@@ -1,5 +1,5 @@
-import express from "express";
-const router = express.Router();
+import { Router } from "express";
+const router = Router();
 import cryptoRandomString from "crypto-random-string";
 import { genSalt, hash } from "bcryptjs";
 import {
--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ import cookieSession from "cookie-session";
 import path from "path";
 import welcomeRouter from "./routes/welcome";
 
-const app: Express = (exports.app = express());
+const app = (exports.app = express());
 const cookieSessionMiddleware = cookieSession({
     secret: `I'm always hungry.`,
     maxAge: 1000 * 60 * 60 * 24 * 90,
